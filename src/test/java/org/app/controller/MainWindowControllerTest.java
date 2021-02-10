87fc515b292e4d6b9d4c0a1f01b7bf11ba7bec57
mglowinski93 <mglowinski93@gmail.com>
2021-02-10 19:47:42 +0100

    tests draw

---
+++ src/test/java/org/app/controller/MainWindowControllerTest.java
@@ -0,0 +1,13 @@
+package org.app.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainWindowControllerTest {
+
+    @Test
+    void MainWindowControllerTest(){
+        Assertions.assertTrue(true);
+    }
+
+}
